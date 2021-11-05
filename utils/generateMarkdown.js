1e3ad4802d16aba5c94d3002b246a51e78934c06
Jeff Korby <82245490+jeffkorby@users.noreply.github.com>
2021-11-05 13:47:10 -0400

    license functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,7 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if(license) {
-    return `![${license} ](https://img.shields.io/badge/license-${license.split(' ').join('%20')}-blue)`
+    return `![badge](https://img.shields.io/badge/license-${license}-blue)`;
   } else {
     return '';
   }
@@ -11,12 +11,26 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+  if(!license) {
+    return '';
+  } else {
+    return `\n* [License](#license)\n`;
+  }
   
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license) {
+    return `## [License](#table-of-contents)
+  The application is covered under:
+  ${renderLicenseLink(license)}
+    `;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
